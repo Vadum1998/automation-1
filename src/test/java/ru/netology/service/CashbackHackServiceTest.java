65259e0729f98940e232a7ed5504ad089c25a904
Вадим Дубровин <vy831158@gmail.com>
2022-06-05 18:44:36 +0300

    Pravka

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -27,8 +27,8 @@ public class CashbackHackServiceTest {
 
     @org.junit.Test
     public void maximumBuy() {
-        int amount = 1000;
-        int expected = 0;
+        int amount = 900;
+        int expected = 100;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
         assertEquals(expected, actual);
